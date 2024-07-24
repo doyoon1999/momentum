069d000056616b034a1e716a8fa2409c2bcb1969
doyoon1999 <kimdoyoon1999@gmail.com>
2024-07-24 18:56:49 +0900

    debug searchForm (about input.value : blank)

--- js/search-form.js
+++ js/search-form.js
@@ -1,23 +1,29 @@
 function searchGoogle(event) {
   event.preventDefault();
-  const query = document.getElementById("searchQuery").value;
+  const queryInput = document.getElementById("searchQueryGoogle");
+  const query = queryInput.value;
   const encodedQuery = encodeURIComponent(query);
   const googleSearchURL = `https://www.google.com/search?q=${encodedQuery}`;
   window.open(googleSearchURL, "_blank");
+  queryInput.value = "";
 }
 
 function searchNaver(event) {
   event.preventDefault();
-  const query = document.getElementById("searchQuery").value;
+  const queryInput = document.getElementById("searchQueryNaver");
+  const query = queryInput.value;
   const encodedQuery = encodeURIComponent(query);
   const naverSearchURL = `https://search.naver.com/search.naver?query=${encodedQuery}`; // 네이버 검색 URL을 생성합니다.
   window.open(naverSearchURL, "_blank");
+  queryInput.value = "";
 }
 
 function searchYouTube(event) {
   event.preventDefault();
-  const query = document.getElementById("searchQuery").value;
+  const queryInput = document.getElementById("searchQueryYoutube");
+  const query = queryInput.value;
   const encodedQuery = encodeURIComponent(query);
   const youtubeSearchURL = `https://www.youtube.com/results?search_query=${encodedQuery}`; // 유튜브 검색 URL을 생성합니다.
   window.open(youtubeSearchURL, "_blank");
+  queryInput.value = "";
 }
